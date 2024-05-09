11a5a1706a81a2a1e6380e42efe02a41ab16c959
colincummins <all.our.august.sundays@gmail.com>
2024-05-09 09:49:27 -0700

    Add message_to_json to parse incoming serialized json messages and provide error messages when necessary

--- server.py
+++ server.py
@@ -4,6 +4,17 @@ import json
 import zmq
 import base64
 from PIL import Image
+
+def message_to_json(message:str)->dict:
+    try:
+        return json.loads(message)
+    except json.JSONDecodeError:
+        return {'status': 'error', 'payload': 'Message is not JSON'}
+    except:
+        return {'status': 'error', 'payload': 'Something went wrong'}
+
+
+
 class Server:
     def __init__(self, port:int=5555):
         self.port = str(port)
@@ -14,14 +25,20 @@ class Server:
     def mainloop(self):
         while True:
             message = self.socket.recv()
-            with open("received_image.jpg","wb+") as img_file:
-                img_file.write(base64.b64decode(message))
-            image_to_convert = Image.open("received_image.jpg")
-            image_to_convert.save("received_image.pdf","PDF")
-            with open("received_image.pdf", "rb") as pdf_data:
-                encoded_pdf = base64.b64encode(pdf_data.read())
+            message = message_to_json(message)
+            if message['status'] == 'error':
+                print("Malformed message")
+            else:
+                with open("received_image.jpg","wb+") as img_file:
+                    img_file.write(base64.b64decode(message['payload']))
+                image_to_convert = Image.open("received_image.jpg")
+                image_to_convert.save("received_image.pdf","PDF")
+                with open("received_image.pdf", "rb") as pdf_data:
+                    encoded_pdf = base64.b64encode(pdf_data.read())
 
-            self.socket.send(encoded_pdf)
+                message = {"status": "success", "payload": encoded_pdf}
+
+            self.socket.send_string(json.dumps(message))
 
 if __name__ == "__main__":
     server = Server(5555)
