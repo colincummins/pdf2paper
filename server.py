81f3bebb1c301d1fb6a145a199dd64fe85790538
Colin Cummins <all.our.august.sundays@gmail.com>
2024-05-09 17:20:37 -0700

    Use ZMQ.send_json to simplify sending. No need to serialize.

--- server.py
+++ server.py
@@ -5,13 +5,6 @@ import zmq
 import base64
 from PIL import Image
 
-def message_to_json(message:str)->dict:
-    try:
-        return json.loads(message)
-    except json.JSONDecodeError:
-        return {'status': 'error', 'payload': 'Message is not JSON'}
-    except:
-        return {'status': 'error', 'payload': 'Something went wrong'}
 
 
 
@@ -24,21 +17,17 @@ class Server:
 
     def mainloop(self):
         while True:
-            message = self.socket.recv()
-            message = message_to_json(message)
-            if message.get('status') == 'error':
-                print("Malformed message")
-            else:
-                with open("received_image.jpg","wb+") as img_file:
-                    img_file.write(base64.b64decode(message['payload']))
-                image_to_convert = Image.open("received_image.jpg")
-                image_to_convert.save("received_image.pdf","PDF")
-                with open("received_image.pdf", "rb") as pdf_data:
-                    encoded_pdf = base64.b64encode(pdf_data.read()).decode('utf-8')
+            message = self.socket.recv_json()
+            with open("received_image.jpg","wb+") as img_file:
+                img_file.write(base64.b64decode(message['payload']))
+            image_to_convert = Image.open("received_image.jpg")
+            image_to_convert.save("received_image.pdf","PDF")
+            with open("received_image.pdf", "rb") as pdf_data:
+                encoded_pdf = base64.b64encode(pdf_data.read()).decode('utf-8')
 
-                message = {"status": "success", "payload": encoded_pdf}
+            message = {"status": "success", "payload": encoded_pdf}
 
-            self.socket.send_string(json.dumps(message))
+            self.socket.send_json(message)
 
 if __name__ == "__main__":
     server = Server(5555)
