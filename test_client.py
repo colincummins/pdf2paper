73049938f73810aa7f4a07bec03d6519186b7a40
Colin Cummins <all.our.august.sundays@gmail.com>
2024-05-09 17:21:35 -0700

    Use ZMQ.send_json and recv_json to simplify sending/receiving. No need to serialize.

--- test_client.py
+++ test_client.py
@@ -1,26 +1,27 @@
 import json
 import zmq
 import base64
+
+
 class TestClient:
-    def __init__(self,address:str="localhost", port:int=5555):
+    def __init__(self, address: str = "localhost", port: int = 5555):
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REQ)
         self.socket.connect("tcp://{}:{}".format(address, port))
 
     def mainloop(self):
-        with open("longcat.jpg","rb") as image_file:
+        with open("longcat.jpg", "rb") as image_file:
             encoded_message = base64.b64encode(image_file.read()).decode('utf-8')
-        message = {"type":"jpg", "payload": encoded_message}
-        message = json.dumps(message)
-        self.socket.send_string(message)
+        message = {"type": "jpg", "payload": encoded_message}
+        self.socket.send_json(message)
 
         encoded_message = self.socket.recv()
-        with open("longcat.pdf","wb+") as pdf_file:
-            decoded_file = base64.b64decode((encoded_message))
+        with open("longcat.pdf", "wb+") as pdf_file:
+            decoded_file = base64.b64decode(encoded_message)
             pdf_file.write(decoded_file)
         print("PDF received")
+
 
 if __name__ == "__main__":
     client = TestClient("localhost", 5555)
     client.mainloop()
-
