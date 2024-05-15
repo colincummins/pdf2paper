75a76e9455bf1e7df04cb5193a17c767c353b929
Colin Cummins <all.our.august.sundays@gmail.com>
2024-05-14 18:20:42 -0700

    Refactor img to pdf to take the whole dictionary as an argument and pick out the kwargs it needs.

--- img_to_pdf.py
+++ img_to_pdf.py
@@ -2,16 +2,16 @@ from PIL import Image
 import base64
 
 
-def img_to_pdf(image_data: str) -> str:
+def img_to_pdf(payload, **kwargs) -> str:
     """
     Takes base64 encoded image file dump and converts to pdf.
     Returns base64 encoded file dump of resulting pdf file
-    :param image_data: Base64 encoded image file dump
+    :param payload: Base64 encoded image file dump
     :return: Base64 encoded file dump of resulting pdf
     """
 
     with open("received_image.jpg", "wb+") as img_file:
-        img_file.write(base64.b64decode(image_data))
+        img_file.write(base64.b64decode(payload))
     image_to_convert = Image.open("received_image.jpg")
     image_to_convert.save("received_image.pdf", "PDF")
     with open("received_image.pdf", "rb") as pdf_data:
