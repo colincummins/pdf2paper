0f1445ad10a623c34068413bfd90cff30f7e6594
Colin Cummins <all.our.august.sundays@gmail.com>
2024-05-16 19:43:27 -0700

    Configure test client for demonstration - text to pdf only, with more progress messages

--- test_client.py
+++ test_client.py
@@ -9,6 +9,7 @@ class TestClient:
         self.socket.connect("tcp://{}:{}".format(address, port))
 
     def mainloop(self):
+        """
         # Test image conversion
         with open("longcat.jpg", "rb") as image_file:
             encoded_message = base64.b64encode(image_file.read()).decode('utf-8')
@@ -22,21 +23,28 @@ class TestClient:
             decoded_file = base64.b64decode(reply['payload'])
             pdf_file.write(decoded_file)
         print("Image PDF received")
+        """
 
         # Test text conversion
         with open("lorem_ipsum.txt", "rb") as text_file:
             encoded_message = base64.b64encode(text_file.read()).decode('utf-8')
         message = {"type": "text", "payload": encoded_message, "font": "Arial", "size": "15"}
+        print("Sending message to PDFMaker Server...")
+        print(json.dumps(message,indent=2))
         self.socket.send_json(message)
 
+        print("")
+
         reply: dict = self.socket.recv_json()
+        print("Receiving reply from server...")
         print(json.dumps(reply, indent=2))
+        print("")
 
 
         with open("lorem_ipsum.pdf", "wb+") as pdf_file:
             decoded_file = base64.b64decode(reply['payload'])
             pdf_file.write(decoded_file)
-        print("Text PDF received")
+        print("Text PDF received: lorem_ipsum.pdf")
 
 
 if __name__ == "__main__":
